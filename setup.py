51568983558221cad9d27161904ae120e2fd14ad
maddy131 <maitraee131@gmail.com>
2025-02-07 10:21:05 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version = '0.0.0',
+    author = 'Maitraee Mitra',
+    author_email='maitraee131@gamil.com',
+    packages= find_packages(),
+    install_requires = []
+)
